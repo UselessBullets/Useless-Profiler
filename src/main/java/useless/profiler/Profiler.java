f34c97b3b65e03f94c36c9ec614cb5365f622069
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-10-16 10:17:24 -0500

    modID based method tracking

--- src/main/java/useless/profiler/Profiler.java
+++ src/main/java/useless/profiler/Profiler.java
@@ -2,6 +2,7 @@ package useless.profiler;
 
 import net.fabricmc.api.ModInitializer;
 import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.ApiStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,9 +19,17 @@ public class Profiler implements ModInitializer {
 	protected static HashMap<String, Integer> methodCalls = new HashMap<>();
 	protected static HashMap<String, Integer> longestTimes = new HashMap<>();
 	private static int longestKey = 0;
+
 	/**
 	 * Returns the average time that the specified method takes to run in milliseconds
 	 */
+	public static float getAverageTime(String modID, String methodID){
+		return getAverageTime(modID + "-" + methodID);
+	}
+	/**
+	 * Returns the average time that the specified method takes to run in milliseconds
+	 */
+	@ApiStatus.Internal
 	public static float getAverageTime(String id){if (!doProfiling){
 		return -1;}
 		return ((float) ellapsedTime.get(id)) /methodCalls.get(id);
@@ -28,6 +37,13 @@ public class Profiler implements ModInitializer {
 	/**
 	 * Marks the start point of a specified method
 	 */
+	public static void methodStart(String modID, String methodID){
+		methodStart(modID + "-" + methodID);
+	}
+	/**
+	 * Marks the start point of a specified method
+	 */
+	@ApiStatus.Internal
 	public static void methodStart(String id){
 		if (!doProfiling){
 			return;}
@@ -46,6 +62,13 @@ public class Profiler implements ModInitializer {
 	/**
 	 * Marks the end point of a specified method
 	 */
+	public static void methodEnd(String modID, String methodID){
+		methodEnd(modID + "-" + methodID);
+	}
+	/**
+	 * Marks the end point of a specified method
+	 */
+	@ApiStatus.Internal
 	public static void methodEnd(String id){
 		if (!doProfiling){
 			return;}
@@ -69,6 +92,9 @@ public class Profiler implements ModInitializer {
 			builder.append(key).append("\t\t\t").append(getAverageTime(key)).append("\t").append(ellapsedTime.get(key)).append("\t").append(methodCalls.get(key)).append("\n");
 		}
 		LOGGER.info(builder.toString());
+	}
+	public static void printTimesInRespectToID(String modID, String methodID){
+		printTimesInRespectToID(modID + "-" + methodID);
 	}
 	/**
 	 * Prints out each methods times in respect to the method provided
